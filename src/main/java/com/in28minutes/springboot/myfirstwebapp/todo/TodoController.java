908df165da2c6ba536723dae68acc7ca994b37a0
NahomAl <nahomalem95@gmail.com>
2023-12-21 14:57:35 +0300

    i added a todo but setting target date is not made yet but it is by default 1 month

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,11 +3,15 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
     TodoService todoService;
 
@@ -15,10 +19,20 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    @RequestMapping("list-todos")
+    @RequestMapping(value = "list-todos", method = RequestMethod.GET)
     public String listAllTodos(ModelMap model){
-        List<Todo> todos = todoService.findByUser("Nahum");
+        List<Todo> todos = todoService.findByUser("Nahom");
         model.addAttribute("todos",todos);
         return "listTodo";
+    }
+    @RequestMapping(value = "addTodo", method = RequestMethod.GET)
+    public String showNewTodos(){
+        return "todo";
+    }
+    @RequestMapping(value = "addTodo", method = RequestMethod.POST)
+    public String AddNewTodos(ModelMap model, @RequestParam String description){
+        String username = (String)model.get("name");
+        todoService.addTodo(username,description, LocalDate.now().plusMonths(1),false);
+        return "redirect:list-todos";
     }
 }
--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -8,10 +8,14 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todo = new ArrayList<>();
+    private static int count = 0;
     static {
-        todo.add(new Todo(1,"Nahom","Go home", LocalDate.now().plusDays(1),false));
-        todo.add(new Todo(2,"Abel","Move out", LocalDate.now().plusDays(28),false));
-        todo.add(new Todo(3,"Bereketi","Work out", LocalDate.now().plusDays(8),false));
+        todo.add(new Todo(++count,"Nahom","Go home", LocalDate.now().plusDays(1),false));
+        todo.add(new Todo(++count,"Abel","Move out", LocalDate.now().plusDays(28),false));
+        todo.add(new Todo(++count,"Bereketi","Work out", LocalDate.now().plusDays(8),false));
+    }
+    public void addTodo(String username,String description,LocalDate targetDate,boolean done){
+        todo.add(new Todo(++count,username,description, targetDate,done));
     }
     public List<Todo> findByUser(String username){
         return todo;
